25cd6de35b00406b84fd664f1f82c8e3102fbae0
Laura Aupert <aupert.laura@gmail.com>
2021-04-10 11:48:29 -0400

    Created API.js file and coded the query for RAWG. Installed Axios and Passport.

---
+++ client/src/utils/API.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+// Export an object containing methods we'll use for accessing the Wikipedia API
+
+export default {
+  searchTerms: function(query) {
+    return axios.get(
+      "https://api.rawg.io/api/games?key=" + REACT_APP_APIKEY + "&search=" + query 
+        // "&limit=1&format=json&origin=*"
+    );
+  }
+};
+// GET https://api.rawg.io/api/games?key=YOUR_API_KEY&dates=2019-09-01,2019-09-30&platforms=18,1,7
