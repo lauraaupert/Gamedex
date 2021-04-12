206054cbc1dcb86fc0371ae1805ed618633ce9e1
Purna Gurung <purnagurung903@gmail.com>
2021-04-12 13:26:24 -0400

    added link for forgot password and signup

--- client/src/components/Wishlist/login.js
+++ client/src/components/Wishlist/login.js
@@ -1,4 +1,4 @@
-import { Grid, Paper, Avatar, TextField, Button,  } from "@material-ui/core";
+import { Grid, Paper, Avatar, TextField, Button, Typography, Link  } from "@material-ui/core";
 import React from "react";
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -6,13 +6,13 @@ import Checkbox from '@material-ui/core/Checkbox';
 const Login = () => {
   const paperStyle={padding :20,height:"70vh",width:280, margin:"20px auto"}
   const avatarStyle={backgroundColor:"#00801c"}
-  const buttonStyle={backgroundColor:"#00801c"}
+  const btnStyle={backgroundColor:"#00801c", margin: "8px 0"}
   return(
     <Grid>
       <Paper elevation={10} style={paperStyle}>
         <Grid align="center">
         <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
-       <h2>Login in</h2>
+       <h2>Login</h2>
        </Grid>
        <TextField label="Username" placeholder="Enter username" fullWidth required />
        <TextField label="Password" placeholder="Enter Password" type="password" fullWidth required />
@@ -25,9 +25,19 @@ const Login = () => {
         }
         label="Remember me"
       />
-        <Button type="submit" style={buttonStyle} variant="contained" color="secondary" fullWidth>
-        Login in 
+        <Button type="submit" style={btnStyle} variant="contained" color="secondary" fullWidth>
+        Login 
         </Button>
+        <Typography > 
+        <Link href="#" >
+          Forgot password ?
+        </Link>
+        </Typography>
+        <Typography > Do you have an account ?
+        <Link href="#" > 
+           Sign up!
+        </Link>
+        </Typography>
       </Paper>
     </Grid>
   )
