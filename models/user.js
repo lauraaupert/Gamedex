5fe58091a75ae4363b90623b78c791ef23907a86
Laura Aupert <aupert.laura@gmail.com>
2021-04-14 20:18:38 -0400

    Added code in models/index.js

--- models/user.js
+++ models/user.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
-const User = new Schema({});
+const User = new Schema({
+    //DEFINE USER HERE
+});
 
 User.plugin(passportLocalMongoose);
 
