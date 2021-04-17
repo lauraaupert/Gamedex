0d6a53bc271594a06a4446a3c5fde813fd8f979b
Purna Gurung <purnagurung903@gmail.com>
2021-04-17 14:00:50 -0400

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,29 @@ import React from "react";
 import "./App.css";
 import Wishlist from "./components/Wishlist/WishList";
 import Navbar from "./components/Wishlist/Navbar";
-
 import SignInOutContainer from "./components/containers";
+import { BrowserRouter as Router, Route,  Switch } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div>
+  <Router>
+    
+      <Switch>
       <div className="App">
+        <Route exact path="/">
       <SignInOutContainer />
-      </div>
+      </Route>
+     
+      <Route  exact path={["/Navbar","/Wishlist"]}>
       <Navbar />
       <Wishlist />
-    </div>
+      </Route>
+      </div>
+      </Switch>
+    
+    </Router>
   );
 }
 
