9814198bd5de4e78c9789db13b25f617701e7bc8
Laura Aupert <aupert.laura@gmail.com>
2021-04-19 13:32:15 -0400

    Still trying to fix heroku

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import "./App.css";
 import Wishlist from "./components/Wishlist/WishList";
 import Navbar from "./components/Wishlist/Navbar";
 import SignInOutContainer from "./components/containers";
-import VideoBg from "./components/VideoBg";
+// import VideoBg from "./components/VideoBg";
 
 function App() {
   return (
     <div>
       <div className="App">
-      <VideoBg />
+      {/* <VideoBg /> */}
       <SignInOutContainer />
       </div>
       <Navbar />
