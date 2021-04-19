ed96b6a98045dcf51b76eea8554fc9ba239f18ad
Laura Aupert <aupert.laura@gmail.com>
2021-04-19 18:57:02 -0400

    Added Navbar to noMatch.js

--- client/src/pages/noMatch.js
+++ client/src/pages/noMatch.js
@@ -1,26 +1,18 @@
 import React from "react";
-// import { Col, Row, Container } from "../components/Grid";
-// import Jumbotron from "../components/Jumbotron";
+import Navbar from "../components/NavBar/index.js";
 
 function NoMatch() {
   return (
       <div>
-    {/* // <Container fluid>
-    //   <Row>
-    //     <Col size="md-12"> */}
-          {/* <Jumbotron> */}
+          <Navbar />
             <h1>404 Page Not Found</h1>
             <h1>
               <span role="img" aria-label="Face With Rolling Eyes Emoji">
                 🙄
               </span>
             </h1>
-          {/* </Jumbotron> */}
-    {/* //     </Col>
-    //   </Row>
-    // </Container> */}
-    </div>
-  );
+        </div>
+    );
 }
 
 export default NoMatch;
