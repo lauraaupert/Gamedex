2fac3357587f1cda84b2a8e1cd840390477a5a64
Matthew Vanni <mvanni4994@gmail.com>
2021-04-22 11:39:56 -0400

    what?

---
+++ client/build/precache-manifest.36d8c78abf2163516afe45014706c4ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa523f204da5d3e1d955d17f248980a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ae682160488f72d80dc",
+    "url": "/static/css/main.1ec779d2.chunk.css"
+  },
+  {
+    "revision": "7d9306460eabcb4d71ad",
+    "url": "/static/js/2.79c5c7a6.chunk.js"
+  },
+  {
+    "revision": "5cf0b55f711b4765b7e42faa5df6636d",
+    "url": "/static/js/2.79c5c7a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae682160488f72d80dc",
+    "url": "/static/js/main.d95207c8.chunk.js"
+  },
+  {
+    "revision": "8d8751a9d5c41ae39bbc",
+    "url": "/static/js/runtime-main.98cfd11d.js"
+  },
+  {
+    "revision": "37d48bb4162539c77f5c86c0d66a5228",
+    "url": "/static/media/gameMontage.37d48bb4.mp4"
+  }
+]);
