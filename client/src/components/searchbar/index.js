3bb08fb5872444807a646e9003de95a5737eb842
Rajahni fields <rajahnifields@gmail.com>
2021-04-22 21:47:52 -0400

    fixed gametable

--- client/src/components/searchbar/index.js
+++ client/src/components/searchbar/index.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
+import InputBase from "@material-ui/core/InputBase";
 import { fade, makeStyles } from "@material-ui/core/styles";
-
+import SearchIcon from "@material-ui/icons/Search";
 import { Button, AppBar, Toolbar } from "@material-ui/core";
-import NavBar from "../navbar/index"
-// import GameTable from "../Table/Table";
-// import Autocomplete from "@material-ui/lab/Autocomplete";
+import NavBar from "../NavBar/index";
+import API from "../../utils/API";
+import axios from "axios";
+import GameTable from "../Table";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
   title: {
     flexGrow: 1,
     display: "none",
@@ -18,32 +22,51 @@ const useStyles = makeStyles((theme) => ({
       display: "block",
     },
   },
-
   search: {
     position: "relative",
     borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
+    backgroundColor: fade(theme.palette.common.white, 0.5),
     "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
+      backgroundColor: fade(theme.palette.common.white, 0.5),
     },
     marginLeft: 0,
     width: "100%",
     [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(1),
       width: "auto",
+      marginTop: 150,
     },
   },
-
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   inputRoot: {
     color: "inherit",
   },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
 }));
 
-const login = [
-  { id: 1, cost: "Snow", Name: "Jon", platform: 35 },
-  { id: 2, cost: "Rain", Name: "Jess", platform: 50 },
-];
-
+// function gameCall {
+//  searchTerms
+// }
 export default function SearchBar() {
   const classes = useStyles();
   const btnStyle = { backgroundColor: "#00801c", margin: "8px 0" };
@@ -52,173 +75,72 @@ export default function SearchBar() {
     color: "black",
     fontvariant: "all-petite-caps",
   };
+  const login = [
+    // { id: 1, cost: "Snow", Name: "Jon", platform: 35 },
+    // { id: 2, cost: "Rain", Name: "Jess", platform: 50 },
+  ];
+  const gameInput = [
+    // { id: 1, cost: "Snow", Name: "Jon", platform: 35 },
+    { id: 2, cost: "Rain", Name: "Jess", platform: 50 },
+  ];
 
   return (
     <div>
       {login.length ? (
         <NavBar />
       ) : (
-        <AppBar position="static">
-          <Toolbar style={NavStlye}>
-            <Button
-              type="submit"
-              style={btnStyle}
-              variant="contained"
-              color="secondary"
-              halfWidth
-              href="/login"
-              mergin="right"
-            >
-              Login
-            </Button>
-          </Toolbar>
-        </AppBar>
+        <>
+          <AppBar position="static">
+            <Toolbar style={NavStlye}>
+              <Button
+                type="submit"
+                style={btnStyle}
+                variant="contained"
+                color="secondary"
+                href="/login"
+                mergin="right"
+              >
+                Login
+              </Button>
+            </Toolbar>
+          </AppBar>
+        </>
       )}
       <br></br>
-      <div style={classes}>
-        <form action="/" method="get">
-          <label htmlFor="header-search">
-            <span className="visually-hidden">Game Choice</span>
-          </label>
-          <br></br>
-          <br></br>
-          <input
-            type="text"
-            id="header-search"
-            placeholder="Search Games"
-            name="s"
-          />
-          <button type="submit">Search</button>
-        </form>
+
+      <div className="form-group">
+        <div className={classes.root}>
+          <div className={classes.search}>
+            <h1>Search for a game</h1>
+            {/* <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div> */}
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+            <button
+              onClick={API.getGame}
+              id="Searchy"
+              className="btn btn-primary mt-3"
+            >
+              Search
+            </button>
+          </div>
+        </div>
       </div>
-      {/* <GameTable /> */}
+
+      <br></br>
+
+      {gameInput.length ? (
+        <GameTable rows={gameInput} />
+      ) : (
+        <h1>Nothing is in here go away</h1>
+      )}
     </div>
   );
 }
-
-
-
-
-// import React from "react";
-// import InputBase from "@material-ui/core/InputBase";
-// import { fade, makeStyles } from "@material-ui/core/styles";
-// import SearchIcon from "@material-ui/icons/Search";
-// import { Button, AppBar, Toolbar } from "@material-ui/core";
-// import NavBar from "../navbar/index";
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     flexGrow: 1,
-//   },
-//   menuButton: {
-//     marginRight: theme.spacing(2),
-//   },
-//   title: {
-//     flexGrow: 1,
-//     display: "none",
-//     [theme.breakpoints.up("sm")]: {
-//       display: "block",
-//     },
-//   },
-//   search: {
-//     position: "relative",
-//     borderRadius: theme.shape.borderRadius,
-//     backgroundColor: fade(theme.palette.common.white, .50),
-//     "&:hover": {
-//       backgroundColor: fade(theme.palette.common.white, .50),
-//     },
-//     marginLeft: 0,
-//     width: "100%",
-//     [theme.breakpoints.up("sm")]: {
-//       marginLeft: theme.spacing(1),
-//       width: "auto",
-//       marginTop: 400,
-//     },
-//   },
-//   searchIcon: {
-//     padding: theme.spacing(0, 2),
-//     height: "100%",
-//     position: "absolute",
-//     pointerEvents: "none",
-//     display: "flex",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   inputRoot: {
-//     color: "inherit",
-//   },
-//   inputInput: {
-//     padding: theme.spacing(1, 1, 1, 0),
-//     // vertical padding + font size from searchIcon
-//     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-//     transition: theme.transitions.create("width"),
-//     width: "100%",
-//     [theme.breakpoints.up("sm")]: {
-//       width: "12ch",
-//       "&:focus": {
-//         width: "20ch",
-//       },
-//     },
-//   },
-// }));
-// const login = [
-//   //   { id: 1, cost: "Snow", Name: "Jon", platform: 35 },
-//   //   { id: 2, cost: "Rain", Name: "Jess", platform: 50 },
-// ];
-// export default function SearchBar() {
-//   const classes = useStyles();
-//   const btnStyle = { backgroundColor: "#00801c", margin: "8px 0" };
-//   const NavStlye = {
-//     background: "darkgreen",
-//     color: "black",
-//     fontvariant: "all-petite-caps",
-//   };
-//   return (
-//     <div>
-//       {login.length ? (
-//         <NavBar />
-//       ) : (
-//         <AppBar position="static">
-//           <Toolbar style={NavStlye}>
-//             <Button
-//               type="submit"
-//               style={btnStyle}
-//               variant="contained"
-//               color="secondary"
-//               href="/login"
-//               mergin="right"
-//             >
-//               Login
-//             </Button>
-//           </Toolbar>
-//         </AppBar>
-//       )}
-//       <br></br>
-
-//       <div className="form-group">
-//         <div className={classes.root}>
-//           <div className={classes.search}>
-//             <h1>Search for a game</h1>
-//             <div className={classes.searchIcon}>
-//               <SearchIcon />
-//             </div>
-//             <InputBase
-//               placeholder="Search…"
-//               classes={{
-//                 root: classes.inputRoot,
-//                 input: classes.inputInput,
-//               }}
-//               inputProps={{ "aria-label": "search" }}
-//             />
-//             <button
-//               //   onClick={props.handleFormSubmit}
-//               className="btn btn-primary mt-3"
-//             >
-//               Search
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,6 +1,6 @@
 import React from "react";
 import SearchBar from "../components/searchbar";
-import GameTable from "../components/Table";
+// import GameTable from "../components/Table";
 
 function Landing() {
   return (
