dd81e9f7cf51bf8d84872809a7fe2f21faa3e35b
Laura Aupert <aupert.laura@gmail.com>
2021-04-23 01:06:44 -0400

    linked the games to their user

--- models/user.js
+++ models/user.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Game = require("./Game");
 // const passportLocalMongoose = require('passport-local-mongoose');
 
 const User = new Schema({
   email: String, 
   password: String,
-  name: String
+  name: String,
+  games: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Game",
+    }
+  ]
     //DEFINE USER HERE
 
 });
